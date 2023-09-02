9f51155686cfb85a65c1eee5b7edf44e8f3d2ce9
Aswin Sam M <aswinsam2810@gmail.com>
2023-09-02 12:50:42 +0530

    done fibonacci and command line arguments in assignment 1

---
+++ Assignment1/commandLine.java
@@ -0,0 +1,9 @@
+package Assignment1;
+
+public class commandLine {
+    public static void main(String args[]){
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+    }
+}
